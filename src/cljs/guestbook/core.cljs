202d2b774298f6da9df01787971f9c613875085d
Willy <willy.heng@gmail.com>
2021-01-08 20:54:17 +0000

    C4: Migrated to Reframe

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -1,16 +1,44 @@
 (ns guestbook.core
   (:require [reagent.core :as r]
             [reagent.dom :as dom]
+            [re-frame.core :as rf]
             [ajax.core :refer [GET POST]]
             [clojure.string :as string]
             [guestbook.validation :refer [validate-message]]))
 
-(defn get-messages [messages]
+(rf/reg-event-fx
+ :app/initialize
+ (fn [_ _]
+   {:db {:messages/loading? true}}))
+
+(rf/reg-sub
+ :messages/loading?
+ (fn [db _]
+   (:messages/loading? db)))
+
+(rf/reg-event-db
+ :messages/set
+ (fn [db [_ messages]]
+   (-> db
+       (assoc :messages/loading? false
+              :messages/list messages))))
+
+(rf/reg-sub
+ :messages/list
+ (fn [db _]
+   (:messages/list db [])))
+
+(rf/reg-event-db
+ :message/add
+ (fn [db [_ message]]
+   (update db :messages/list conj message)))
+
+(defn get-messages []
   (GET "/messages"
        {:headers {"Accept" "application/transit+json"}
-        :handler #(reset! messages (:messages %))}))
+        :handler #(rf/dispatch [:messages/set (:messages %)])}))
 
-(defn send-message! [fields errors messages]
+(defn send-message! [fields errors]
   (if-let [validation-errors (validate-message @fields)]
     (reset! errors validation-errors)
     (POST "/message"
@@ -20,7 +48,7 @@
            :params @fields
            :handler #(do
                        (.log js/console (str "response:" %))
-                       (swap! messages conj (assoc @fields :timestamp (js/Date.)))
+                       (rf/dispatch [:message/add (assoc @fields :timestamp (js/Date.))])
                        (reset! fields nil)
                        (reset! errors nil))
            :error-handler #(do (.error js/console (str "error:" %))
@@ -40,7 +68,7 @@
       [:p message]
       [:p " - " name]])])
 
-(defn message-form [messages]
+(defn message-form []
   (let [fields (r/atom {})
         errors (r/atom nil)]
     (fn [] 
@@ -63,19 +91,23 @@
           :on-change #(swap! fields assoc :message (-> % .-target .-value))}]]
        [:input.button.is-primary
         {:type :submit
-         :on-click #(send-message! fields errors messages)
+         :on-click #(send-message! fields errors)
          :value "comment"}]])))
 
 (defn home []
-  (let [messages (r/atom nil)]
-    (get-messages messages)
+  (let [messages (rf/subscribe [:messages/list])]
+    (rf/dispatch [:app/initialize])
+    (get-messages)
     (fn []
-      [:div.content>div.columns.is-centered>div.column.is-two-thirds
-       [:div.columns>div.column
-        [:h3 "Messages"]
-        [message-list messages]]
-       [:div.columns>div.column
-        [message-form messages]]])))
+      (if @(rf/subscribe [:messages/loading?])
+        [:div>div.row>div.span12>h3
+         "Loading Messages..."]
+        [:div.content>div.columns.is-centered>div.column.is-two-thirds
+         [:div.columns>div.column
+          [:h3 "Messages"]
+          [message-list messages]]
+         [:div.columns>div.column
+          [message-form]]]))))
 
 (dom/render
  [home]
