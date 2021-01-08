079f124d93ab4831bb7814b98948a09eb95b1d93
Willy <willy.heng@gmail.com>
2021-01-08 21:36:26 +0000

    C5: Migrated to messages.clj

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -6,7 +6,7 @@
    [guestbook.middleware :as middleware]
    [ring.util.response]
    [ring.util.http-response :as response]
-   [guestbook.validation :refer [validate-message]]))
+   [guestbook.messages :as msg]))
 
 (defn home-page [request]
   (layout/render request "home.html" ))
@@ -15,19 +15,22 @@
   (layout/render request "about.html"))
 
 (defn message-list [_]
-  (response/ok {:messages (vec (db/get-messages))}))
+  (response/ok (msg/message-list)))
 
 (defn save-message! [{:keys [params]}]
-  (if-let [errors (validate-message params)]
-    (response/bad-request {:errors errors})
-    (try
-      (db/save-message! params)
-      (response/ok {:status :ok})
-      (catch Exception e
-        (response/internal-server-error
-         {:errors {:server-error ["Failed to save message!"]}})))))
+  (try
+    (msg/save-message! params)
+    (response/ok {:status :ok})
+    (catch Exception e
+      (let [{id :guestbook/error-id
+             errors :error} (ex-data e)]
+        (case id
+          :validation (response/bad-request {:errors errors})
+          ;;else
+          (response/internal-server-error
+           {:errors {:server-error ["Failed to save message!"]}}))))))
 
-(defn home-routes []
+(defn home-routes [] 
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
