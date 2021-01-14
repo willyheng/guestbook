f1e80b496744077a223c557524ae912b9a9e0d46
Willy <willy.heng@gmail.com>
2021-01-14 22:24:15 +0000

    C7: Shared routes client/server, Refactored Clojurescript

--- src/clj/guestbook/handler.clj
+++ src/clj/guestbook/handler.clj
@@ -2,24 +2,24 @@
   (:require
     [guestbook.middleware :as middleware]
     [guestbook.layout :refer [error-page]]
-    [guestbook.routes.home :refer [home-routes]]
     [reitit.ring :as ring]
     [ring.middleware.content-type :refer [wrap-content-type]]
     [ring.middleware.webjars :refer [wrap-webjars]]
     [guestbook.env :refer [defaults]]
     [mount.core :as mount]
     [guestbook.routes.services :refer [service-routes]]
-    [guestbook.routes.websockets :refer [websocket-routes]]))
+    [guestbook.routes.websockets :refer [websocket-routes]]
+    [guestbook.routes.app :refer [app-routes]]))
 
 (mount/defstate init-app
   :start ((or (:init defaults) (fn [])))
   :stop  ((or (:stop defaults) (fn []))))
 
-(mount/defstate app-routes
+(mount/defstate routes
   :start
   (ring/ring-handler
     (ring/router
-      [(home-routes)
+      [(app-routes)
        (service-routes)
        (websocket-routes)])
     (ring/routes
@@ -36,4 +36,4 @@
          (constantly (error-page {:status 406, :title "406 - Not acceptable"}))}))))
 
 (defn app []
-  (middleware/wrap-base #'app-routes))
+  (middleware/wrap-base #'routes))
